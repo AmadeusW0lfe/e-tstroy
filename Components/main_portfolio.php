a5fec57ddb2d1c9ecf26661e95f2c32a26528585
pentagonich <serwillow.wolfe@gmail.com>
2020-02-03 05:15:48 +0300

    Added admin page and admin enter

--- Components/main_portfolio.php
+++ Components/main_portfolio.php
@@ -1,14 +1,21 @@
 <section class="project-back">
 	<div class="container">
 		<div class="row">
-			<div class="col">
+			<div class="col-6 project-portfolio-back">
 				<section class="project-filter">
 					<p>
 						Выберите категорию
-					</p>
-				</section>	
+					</p>	
+				</section>
+			</div>
+			<div class="col-6 project-portfolio-back input-portfolio">
+				<section>
+						<select name="" id="">
+						<option value="">Жилые дома</option>
+						<option value="">Заводские сооружения</option>
+					</select>	
+					</section>
 			</div>
 		</div>
-		<!-- Добавить слайдер проектов -->
 	</div>
 </section>
